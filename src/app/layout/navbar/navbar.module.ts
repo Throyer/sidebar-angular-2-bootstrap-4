fb1a7b7dbfd964b6d3fae93ace975b54a98cbcb3
Renato Henrique <renato.henrique_98@hotmail.com>
2020-03-21 16:27:36 -0300

    Correção na navbar

--- src/app/layout/navbar/navbar.module.ts
+++ src/app/layout/navbar/navbar.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    BsDropdownModule.forRoot()
+  ],
   exports: [NavbarComponent]
 })
 export class NavbarModule { }
